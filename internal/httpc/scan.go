66f7be391226d457555a8bc86d67ce0e03c6fbfa
adzsx <adzsx@proton.me>
2024-05-04 01:02:44 +0200

    Added progress status

--- internal/httpc/scan.go
+++ internal/httpc/scan.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"strings"
@@ -22,28 +23,52 @@ func Scan(input utils.Input) error {
 
 	if scan.Robots {
 		Robots(scan.Host)
-		log.Println(bleep)
+
+		var count float64 = float64(len(bleep))
+		var done float64
 		for _, entry := range bleep {
 			resp, _ := http.Get(scan.Host + "/" + entry)
+			done++
+
+			fmt.Print("\033[2K\033[999D")
 			if resp.StatusCode != 404 {
-				fmt.Printf("[%v] %v/%v\n", resp.StatusCode, scan.Host, entry)
+
+				fmt.Printf("[%v] /%v\n", resp.StatusCode, entry)
 			}
+
+			fmt.Printf("Progess: %v%v (%v/%v)", math.Round(done/count*1000)/10, "%", done, count)
 		}
 	} else {
 		file, err := os.Open(scan.Wordlist)
-		if err != nil {
-			log.Fatal(err)
-		}
+		utils.Err(err)
+		stat, err := file.Stat()
+		bufSize := stat.Size()
+		utils.Err(err)
 		defer file.Close()
 
 		scanner := bufio.NewScanner(file)
+
+		var count int
+		for scanner.Scan() {
+			count++
+		}
+
+		var done int
+
+		buf := make([]byte, bufSize)
+		scanner.Buffer(buf, int(bufSize))
 		// optionally, resize scanner's capacity for lines over 64K, see next example
 		for scanner.Scan() {
 			resp, _ := http.Get(scan.Host + "/" + scanner.Text())
 
+			done++
+
 			if resp.StatusCode != 404 {
+
 				fmt.Printf("[%v] %v/%v\n", resp.StatusCode, scan.Host, scanner.Text())
 			}
+
+			fmt.Printf("Progess: %v (%v/%v)", math.Round(float64(done/count)), done, count)
 		}
 	}
 
@@ -52,9 +77,7 @@ func Scan(input utils.Input) error {
 
 func Robots(host string) {
 	bleepbloop := host + "/robots.txt"
-	log.Println(bleepbloop)
 	status := Status(bleepbloop)
-	log.Println(status)
 	if status == 200 {
 		resp, _ := http.Get(bleepbloop)
 
@@ -66,7 +89,6 @@ func Robots(host string) {
 		for _, entry := range strings.Split(body, "\n") {
 			ent = utils.FilterChar(entry, ":", false)
 			if len(ent) > 1 {
-				log.Println(string(ent[1]))
 				if string(ent[1]) == "/" {
 
 					bleep = append(bleep, ent[2:])
@@ -75,5 +97,7 @@ func Robots(host string) {
 
 		}
 		utils.Err(err)
+	} else if status == 404 {
+		log.Fatalln("robots.txt does not exist on this host")
 	}
 }
